81782995be014e060935f23f95626c1dd04f40fd
Akhil Nambiar <akhilnambiar@Akhils-MacBook-Pro.local>
2016-05-14 19:35:56 -0700

    adding db

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/db', function (request, response) {
        { response.render('pages/db', {results: result.rows} ); }
     });
   });
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
